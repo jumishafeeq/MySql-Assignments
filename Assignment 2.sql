fbe5b80999f0e4ef53e313eb6ec9167d35df7c19
Jumailath M <jumishafeeq14@gmail.com>
2024-12-14 23:13:17 +0530

    Update Assignment 2.sql

--- Assignment 2.sql
+++ Assignment 2.sql
@@ -18,16 +18,16 @@ rename table Orders to Sales_Orders;
 
 insert into Sales_Orders (Order_Id, Customer_name, Product_Category, Ordered_item, Contact_No, Order_Quantity)
 values
-(1, 'Alice', 'Electronics', 'Smartphone', '8624503157',15),
-(2, 'Rose', 'Electronics', 'Laptop', '9631007891',10),
-(3, 'Charlie', 'Furniture', 'Dining Table', '8634567892',5),
+(1, 'Alice', 'Electronics', 'Smartphone', '8624503157',5),
+(2, 'Rose', 'Electronics', 'Laptop', '9631007891',3),
+(3, 'Charlie', 'Furniture', 'Dining Table', '8634567892',6),
 (4, 'Diana', 'Furniture', 'Sofa Set', '9300567893',3),
-(5, 'Boby', 'Accessories', 'Watch', '8294567894',10),
-(6, 'Roma', 'Accessories', 'Handbag', '8964567895',15),
-(7, 'George', 'Electronics', 'Headphones', '8884567896',30),
+(5, 'Boby', 'Accessories', 'Watch', '8294567894',4),
+(6, 'Roma', 'Accessories', 'Handbag', '8964567895',5),
+(7, 'George', 'Electronics', 'Headphones', '8884567896',6),
 (8, 'Hannah', 'Furniture', 'Office Desk', '9884057897',5),
-(9, 'Marry', 'Accessories', 'Sunglasses', '8994567800',25),
-(10, 'Jack', 'Electronics', 'Smart TV', '8104567899',5);
+(9, 'Marry', 'Accessories', 'Sunglasses', '8994567800',9),
+(10, 'Jack', 'Electronics', 'Smart TV', '8104567899',2);
 
 select * from Sales_Orders;
 
